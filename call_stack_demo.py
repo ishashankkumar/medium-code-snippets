533c290467690edfb05e30b83d81ddac1beb925d
shashank.kumar <shank.git@gmail.com>
2019-11-06 01:04:34 -0800

    Adding code for call_stack_demo

---
+++ call_stack_demo.py
@@ -0,0 +1,24 @@
+"""
+This code snippet was part of story published on thh link
+https://medium.com/cspackets/and-then-i-understood-recursion-c0775125049f
+
+Name of the program :- call_stack_demo.py
+Sample code to demonstrate call stack.
+"""
+
+import traceback
+
+def function_a():
+  return function_b()
+
+def function_b():
+  return function_c()
+
+def function_c():
+  # Prints a stack trace from its invocation point.
+  traceback.print_stack()
+  return "Hi"
+
+if __name__ == "__main__":
+  # Execution starts here.
+  print function_a()
